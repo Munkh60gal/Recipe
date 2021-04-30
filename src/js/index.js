717d13743aefd8225be9eb35a7960ebfa6bd7975
Munkhjargal <munkhjargalbatdavaa@gmail.com>
2021-04-30 16:08:38 +0800

    search iconiig gargana

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
-import {elements} from "./view/base";
+import {elements, renderLoader, clearLoader} from "./view/base";
 import * as searchView from './view/searchView';
 
 /**
@@ -24,10 +24,13 @@ const controlSearch = async () => {
         // 3) Hailt hiihed zoriulj delgets(UI)-iig beltgene.
         searchView.clearSearchQuery();
         searchView.clearSearchResult();
+        renderLoader(elements.searchResultDiv);
+
         // 4) Hailtiig guitsetgene.
         await state.search.doSearch();
 
         // 5) Hailtiin ur dung delgetsend uzuulne.
+        clearLoader();
         if(state.search.result === undefined) alert("Хайлт илэрцгүй...");
         else searchView.renderRecipes(state.search.result);    
     }
--- src/js/view/base.js
+++ src/js/view/base.js
@@ -2,5 +2,31 @@
 export const elements = {
     searchForm: document.querySelector(".search"),  // search hiih uyd
     searchInput: document.querySelector(".search__field"), // search hiihed bichih ug
+    searchResultDiv: document.querySelector(".results"), // results div
     searchResultList: document.querySelector(".results__list") // haisan joriig gargah 
+};
+
+export const elementStrings = {
+    loader: "loader"
+}
+
+// unshaad duussan uyd iconiig ustgah
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    // parentaas ni child element iconig ustgah
+    if(loader) loader.parentElement.removeChild(loader); 
+};
+
+
+// loading iconig uzuleh funkts
+export const renderLoader = parent => {
+    const loader = `
+    <div class="${elementStrings.loader}">
+        <svg>
+            <use href="img/icons.svg#icon-cw"</use>
+        </svg>
+    </div>
+    `;
+
+    parent.insertAdjacentHTML("afterbegin", loader);
 }
